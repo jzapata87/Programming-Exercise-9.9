8601b2f25a18d7a6d60a5c27a64bc1ba5039725f
javierzapata <javierzapata@localhost>
2018-03-20 00:31:12 -0500

    Added Static data and methods.

--- src/testregularpolygon/TestRegularPolygon.java
+++ src/testregularpolygon/TestRegularPolygon.java
@@ -8,10 +8,15 @@ public class TestRegularPolygon {
         RegularPolygon poly1 = new RegularPolygon();
         
         RegularPolygon poly2 = new RegularPolygon(8,12,0.0,0.0);
+        RegularPolygon poly3 = new RegularPolygon(8,12,4.0,2.0);
         
         System.out.println("perimeter is " + poly1.getPerimeter());
-        System.out.println("area is "+ poly2.getArea());
+        System.out.println("area is "+ poly1.getArea());
         System.out.println("center is " + poly1.getCenter().toString());
+        System.out.println();
+        System.out.println("perimeter is " + poly3.getPerimeter());
+        System.out.println("area is "+ poly3.getArea());
+        System.out.println("center is " + poly3.getCenter().toString());
         
         
     }
